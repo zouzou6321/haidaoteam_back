93502cf22a62f03ca8a40dc75578b4171210e9bf
zouzou6321 <692673390@qq.com>
2014-04-25 12:35:05 +0800

    Revert "init"
    This reverts commit 7b8ff8df4867556e9b7d7f255d1f28e79e2cf824.

--- haidaoteam_json/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from haidaoteam_json.models import *
-
-admin.site.register(home)
-admin.site.register(QA)
-admin.site.register(one_list)
--- index.wsgi
+++
@@ -1,4 +0,0 @@
-import sae
-from haidaoteam import wsgi
-
-application = sae.create_wsgi_app(wsgi.application)
